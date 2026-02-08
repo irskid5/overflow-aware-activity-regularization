4b8f470927c04f84fc63c1a9c99dc18fe78fd7a9
Vele Tosevski <vele97@gmail.com>
2026-02-08 12:32:57 -0500

    feat(mnist): update __init__.py with full experiment exports

--- experiments/mnist/__init__.py
+++ experiments/mnist/__init__.py
@@ -1,10 +1,33 @@
 """MNIST RNN experiment with OAR regularization."""
 
 from experiments.mnist.data import get_datasets, normalize_img, resize, augment
+from experiments.mnist.model import get_model
+from experiments.mnist.config import (
+    MNIST_OPTIONS,
+    ENLARGED_MNIST_OPTIONS,
+    get_default_layer_options,
+    get_model_parameter_stats,
+    perform_step_in_four_step_quant,
+    perform_four_step_quant,
+)
+from experiments.mnist.training import configure_environment, train
 
 __all__ = [
+    # Data
     "get_datasets",
     "normalize_img",
     "resize",
     "augment",
+    # Model
+    "get_model",
+    # Config
+    "MNIST_OPTIONS",
+    "ENLARGED_MNIST_OPTIONS",
+    "get_default_layer_options",
+    "get_model_parameter_stats",
+    "perform_step_in_four_step_quant",
+    "perform_four_step_quant",
+    # Training
+    "configure_environment",
+    "train",
 ]

5f940fe10ed37632ed878507c188b225d9d0bf0d
Vele Tosevski <vele97@gmail.com>
2026-02-07 20:58:03 -0500

    feat(oar): create package skeleton

---
+++ oar/__init__.py
@@ -0,0 +1,9 @@
+"""
+Overflow-Aware Activity Regularization (OAR) Package.
+
+A TensorFlow/Keras framework for training quantized neural networks
+with overflow-aware regularization for TFHE inference.
+"""
+
+# This will be populated as we add modules
+__version__ = "0.1.0"
